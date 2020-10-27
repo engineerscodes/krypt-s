abec48d9e3ec744be23c93cc51f164a488363473
engineerscodes <practicalengineers05@gmail.com>
2020-10-27 18:26:19 +0530

    Initial Commit

---
+++ Cryto/src/Maths/modcal.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package Maths;
+
+/**
+ *  @author M.NAVEEN
+ *  RANDOM CODER'S
+ *  Tech/Project Lead Android Club
+ */
+public class modcal {
+
+public int calmod(int n1,int n2)
+{return n1%n2;
+}
+}
+
