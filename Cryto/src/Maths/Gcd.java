e974ef037ad33dc208dc8f9530b33b5ff579e115
engineerscodes <practicalengineers05@gmail.com>
2020-10-27 18:01:01 +0530

    initial Commit

---
+++ Cryto/src/Maths/Gcd.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package Maths;
+import java.util.*;
+/**
+ *  @author M.NAVEEN
+ *  RANDOM CODER'S
+ *  Tech/Project Lead Android Club
+ */
+public class Gcd 
+{
+    int gcd;
+	public Gcd() 
+	{
+     
+	}
+   public int calGcd(int n1,int n2)
+   {  int r1=n1,r2=n2,r=0,q=0;
+	   while(r2>0)
+	   {
+		   q=r1/r2;
+		   r=r1-q*r2;
+		   r1=r2;
+		   r2=r;
+	   }
+	   System.out.println("GCD 0F"+"("+n1+","+n2+")"+"is : "+r1);
+	   return r1;
+   }
+}
+
