8f95a335978807720f797d2d8d92e84bd349223d
engineerscodes <practicalengineers05@gmail.com>
2020-10-27 18:43:08 +0530

    Initial Commit

---
+++ Cryto/src/Maths/GetSetsElement.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package Maths;
+import java.util.*;
+/**
+ *  @author M.NAVEEN
+ *  RANDOM CODER'S
+ *  Tech/Project Lead Android Club
+ */
+public class GetSetsElement 
+{
+
+	public Set  GetElement(int n) 
+	{   Set s=new LinkedHashSet(n+1);
+		for(int i=0;i<=n;i++)
+			s.add(i);
+		System.out.println(s);
+		return s;
+	}
+	
+}
+
